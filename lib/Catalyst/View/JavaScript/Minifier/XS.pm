c853d7b85f529131ad64296d3380e500cbac841a
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2009-10-30 01:14:20 -0500

    style and whitespace changes

--- lib/Catalyst/View/JavaScript/Minifier/XS.pm
+++ lib/Catalyst/View/JavaScript/Minifier/XS.pm
@@ -28,75 +28,75 @@ has subinclude => (
 );
 
 sub process {
-	my ($self,$c) = @_;
+   my ($self,$c) = @_;
 
-	my $path = $self->path;
-	my $variable = $self->stash_variable;
-	my @files = ();
+   my $path = $self->path;
+   my $variable = $self->stash_variable;
+   my @files = ();
 
-	my $original_stash = $c->stash->{$variable};
+   my $original_stash = $c->stash->{$variable};
 
-	# setting the return content type
-	$c->res->content_type('text/javascript');
+   # setting the return content type
+   $c->res->content_type('text/javascript');
 
-	# turning stash variable into @files
-	if ( $c->stash->{$variable} ) {
-		@files = ( ref $c->stash->{$variable} eq 'ARRAY' ? @{ $c->stash->{$variable} } : split /\s+/, $c->stash->{$variable} );
-	}
+   # turning stash variable into @files
+   if ( $c->stash->{$variable} ) {
+      @files = ( ref $c->stash->{$variable} eq 'ARRAY' ? @{ $c->stash->{$variable} } : split /\s+/, $c->stash->{$variable} );
+   }
 
-	# No referer we won't show anything
-	if ( ! $c->request->headers->referer ) {
-		$c->log->debug('javascripts called from no referer sending blank');
-		$c->res->body( q{ } );
-		$c->detach();
-	}
+   # No referer we won't show anything
+   if ( ! $c->request->headers->referer ) {
+      $c->log->debug('javascripts called from no referer sending blank');
+      $c->res->body( q{ } );
+      $c->detach();
+   }
 
-	# If we have subinclude ON then we should run the action and see what it left behind
-	if ( $self->subinclude ) {
-		my $base = $c->request->base;
-		if ( $c->request->headers->referer ) {
-			my $referer = URI->new($c->request->headers->referer);
-			if ( $referer->path ne '/' ) {
-				$c->forward('/'.$referer->path);
-				$c->log->debug('js taken from referer : '.$referer->path);
-				if ( $c->stash->{$variable} ne $original_stash ) {
-					# adding other files returned from $c->forward to @files ( if any )
-					push @files, ( ref $c->stash->{$variable} eq 'ARRAY' ? @{ $c->stash->{$variable} } : split /\s+/, $c->stash->{$variable} );
-				}
-			} else {
-				# well for now we can't get js files from index, because it's indefinite loop
-				$c->log->debug(q{we can't take js from index, it's too dangerous!});
-			}
-		}
-	}
+   # If we have subinclude ON then we should run the action and see what it left behind
+   if ( $self->subinclude ) {
+      my $base = $c->request->base;
+      if ( $c->request->headers->referer ) {
+         my $referer = URI->new($c->request->headers->referer);
+         if ( $referer->path ne '/' ) {
+            $c->forward('/'.$referer->path);
+            $c->log->debug('js taken from referer : '.$referer->path);
+            if ( $c->stash->{$variable} ne $original_stash ) {
+               # adding other files returned from $c->forward to @files ( if any )
+               push @files, ( ref $c->stash->{$variable} eq 'ARRAY' ? @{ $c->stash->{$variable} } : split /\s+/, $c->stash->{$variable} );
+            }
+         } else {
+            # well for now we can't get js files from index, because it's indefinite loop
+            $c->log->debug(q{we can't take js from index, it's too dangerous!});
+         }
+      }
+   }
 
-	my $home = $self->config->{INCLUDE_PATH} || $c->path_to('root');
-	@files = map {
-		$_ =~ s/\.js$//;
-		Path::Class::File->new( $home, $path, "$_.js" );
-	} @files;
+   my $home = $self->config->{INCLUDE_PATH} || $c->path_to('root');
+   @files = map {
+      $_ =~ s/\.js$//;
+      Path::Class::File->new( $home, $path, "$_.js" );
+   } @files;
 
-	# combining the files
-	my @output = ();
-	for my $file ( @files ) {
-		$c->log->debug("loading js file ... $file");
-		open my $in, '<', "$file";
-		for ( <$in> ) {
-			push @output, $_;
-		}
-		close $in;
-	}
+   # combining the files
+   my @output;
+   for my $file (@files) {
+      $c->log->debug("loading js file ... $file");
+      open my $in, '<', $file;
+      for (<$in>) {
+         push @output, $_;
+      }
+      close $in;
+   }
 
-	if ( @output ) {
-		# minifying them if any files loaded at all
-		$c->res->body(
-                   $c->debug
-                      ? join q{ },@output
-                      : minify(join q{ }, @output )
-                );
-	} else {
-		$c->res->body( q{ } );
-	}
+   if ( @output ) {
+      # minifying them if any files loaded at all
+      $c->res->body(
+         $c->debug
+            ? join q{ },@output
+            : minify(join q{ }, @output )
+      );
+   } else {
+      $c->res->body( q{ } );
+   }
 }
 
 1;
